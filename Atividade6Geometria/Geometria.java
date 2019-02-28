09c45249eb8ce4f4f5d550f57ba41d35a6471fba
Samael Pereira Simoes <samael.simoes@gmail.com>
2019-02-27 22:23:26 -0300

    Placed gitignore, and adjusted 6 exercise

--- Atividade6Geometria/Geometria.java
+++ Atividade6Geometria/Geometria.java
@@ -1,6 +1,15 @@
 public class Geometria {
 	public static void main( String[] args ) {
-        Ponto ponto = new Ponto( 5.0, 6.0 );
-        System.out.println( ponto );
+        Ponto p1 = new Ponto( 3.0, 5.0 );
+        Ponto p2 = new Ponto( 5.0, 5.0 );
+        Ponto p3 = new Ponto( 3.0, 3.0 );
+        Ponto p4 = new Ponto( 5.0, 3.0 );
+        
+        Quadrilatero quadri = new Quadrilatero(  p1, p2, p3, p4 );
+
+        System.out.println( p1 );
+        System.out.println( p2 );
+        System.out.println( p3 );
+        System.out.println( p4 );
 	}
 }
---
+++ Atividade6Geometria/Quadrilatero.java
@@ -0,0 +1,19 @@
+public class Quadrilatero {
+    
+    private Ponto p1;
+    private Ponto p2;
+    private Ponto p3;
+    private Ponto p4;
+
+    public Quadrilatero( Ponto p1, Ponto p2, Ponto p3, Ponto p4 ) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+    }
+    
+    public Double area() {
+
+    }
+
+}
