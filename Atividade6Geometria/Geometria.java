546995248b8ca08d74ce33f51e4e8b06cb132d04
Samael Pereira Simoes <samael.simoes@gmail.com>
2019-02-27 21:52:16 -0300

    First commit Exer geometria

---
+++ Atividade6Geometria/Geometria.java
@@ -0,0 +1,6 @@
+public class Geometria {
+	public static void main( String[] args ) {
+        Ponto ponto = new Ponto( 5.0, 6.0 );
+        System.out.println( ponto );
+	}
+}
---
+++ Atividade6Geometria/Ponto.java
@@ -0,0 +1,28 @@
+public class Ponto {
+	private Double x;
+	private Double y;
+	
+	public Ponto( Double x, Double y ) {
+		this.setX(x);
+		this.setY(y);
+	}
+	@Override
+	public String toString() {
+		return "Ponto -x:" + this.x +  " y:" + this.y;
+	}
+	
+	
+	public Double getX() {
+		return x;
+	}
+	public Double getY() {
+		return y;
+	}
+
+	public void setX(Double x) {
+		this.x = x;
+	}
+	public void setY(Double y) {
+		this.y = y;
+	}
+}
