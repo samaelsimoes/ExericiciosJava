cedbce33345147b95621944025728dc49b20be52
Samael Pereira Simoes <samael.simoes@gmail.com>
2019-03-01 20:25:04 -0300

    Exercicio 7 funcionando e ajustado

--- Atividade7Interface/TesteArea.java
+++ Atividade7Interface/TesteArea.java
@@ -1,21 +1,15 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TesteArea {
     public static void main( String[] args ) {
 
-        ArrayList<AreaCalcula> objetos = new ArrayList<AreaCalcula>();
-        
-        Circulo   circulo   = new Circulo(22.5);
-        Quadrado  quadrado  = new Quadrado(40.0);
-        Retangulo retangulo = new Retangulo(15.0, 2.0);
-
-        objetos.add( circulo );
-        objetos.add( quadrado );
-        objetos.add( retangulo );
-
-        System.out.println( circulo.calcularArea() );
-        System.out.println( retangulo.calcularArea() );
-        System.out.println( quadrado.calcularArea() );      
-        
+    	List<AreaCalcula> objetos = Arrays.asList(
+    			new Circulo( 22.5 ),
+                new Quadrado( 40.0 ),
+                new Retangulo( 15.0, 2.0 )
+        );
+             	
+        objetos.forEach( objeto -> System.out.println( objeto.calcularArea() ) );
     }
 }
